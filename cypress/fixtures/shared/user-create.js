7e8c0acadc480d6dd7e16a59693cd2fdaf224236
EBENEZER ASHONG <ebenezer.ashong@amalitech.org>
2022-09-30 08:42:58 +0000

    modifications

--- cypress/fixtures/shared/user-create.js
+++ cypress/fixtures/shared/user-create.js
@@ -1,20 +1,20 @@
 export function createRegistrationData() {
   return {
     testId: new Date().getTime(),
-    email: 'mario.tester+' + new Date().getTime() + '@gmail.com',
+    email: 'ebenezer.ashong+' + new Date().getTime() + '@amalitech.com',
     password: '123456aA_',
     passwordRepeat: '123456aA_',
     salutation: '0',
-    firstName: 'Mario',
-    lastName: 'Tester',
-    street: 'Oberer Graben 1',
+    firstName: 'Ebenezer',
+    lastName: 'Ashong',
+    street: 'Amakroma Street',
     zipCode: '9000',
-    city: 'St. Gallen',
-    language: 'de',
+    city: 'Takoradi',
+    language: 'En',
     rememberMe: false,
     cumulusNr: '',
     newsletter: false,
-    phone: '+41 44 743 72 72',
-    company: 'my Company',
+    phone: '+233 50 970 32 94',
+    company: 'Amalitech',
   }
 }
